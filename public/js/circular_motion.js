404a87984de71b8b73c29820f7b281552709da37
Navezm <navez.martin@gmail.com>
2021-01-05 09:57:12 +0100

    Add file + navbar

---
+++ public/js/circular_motion.js
@@ -0,0 +1,8 @@
+let header = document.querySelector("header");
+
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
